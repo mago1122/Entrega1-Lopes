aa1b4caa6d7fb59d0aa7d772a87fdbb5507e6a5b
Martin Lopes <martin.lopes11@gmail.com>
2022-11-15 20:09:31 -0300

    Vistas y paths creados en appentrega

---
+++ Entrega/appentrega/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from appentrega.views import *
+
+urlpatterns = [
+
+    path("libros/", vista_libros),
+    path("sucursales/", vista_sucursales),
+    path("autores/", vista_autores),
+    path("miembros/", vista_miembros),
+
+]
--- Entrega/appentrega/views.py
+++ Entrega/appentrega/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpResponse
+from appentrega.models import *
 from django.shortcuts import render
 
 # Create your views here.
+
+def vista_inicio(request):
+    return HttpResponse("Estas en el inicio")
+
+def vista_libros(request):
+    return HttpResponse("Estas en la vista: formulario de libros")
+
+def vista_sucursales(request):
+    return HttpResponse("Estas en la vista: formulario de sucursales")
+
+def vista_autores(request):
+    return HttpResponse("Estas en la vista: formulario de autores")
+
+def vista_miembros(request):
+    return HttpResponse("Estas en la vista: formulario de miembros")
