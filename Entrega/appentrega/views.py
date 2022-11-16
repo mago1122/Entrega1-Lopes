1ea999517e225a29e77fe322ca98f00b6131fbda
Martin Lopes <martin.lopes11@gmail.com>
2022-11-15 22:18:59 -0300

    Primera captura de formulario libros exitosa

--- Entrega/appentrega/views.py
+++ Entrega/appentrega/views.py
@@ -9,6 +9,16 @@ def vista_inicio(request):
     return render(request, "appentrega/inicio.html")
 
 def vista_libros(request):
+
+    if request.method == "POST":
+
+        titulo_libro = request.POST["titulo"]
+        autor_libro = request.POST["autor"]
+        precio_libro = request.POST["precio"]
+    
+        libro = Libros(titulo=titulo_libro, autor=autor_libro, precio=precio_libro)
+        libro.save()
+
     return render(request, "appentrega/libros.html")
 
 def vista_sucursales(request):
